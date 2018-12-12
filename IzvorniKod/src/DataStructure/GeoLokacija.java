8ac1a845f45cc2dd97735a594ebc9998f0b9925d
Luka Mrković <luka.mrkovic@fer.hr>
2018-12-12 10:59:59 +0100

    Dodan razred GeoLokacija

---
+++ IzvorniKod/src/DataStructure/GeoLokacija.java
@@ -0,0 +1,25 @@
+package DataStructure;
+
+public class GeoLokacija {
+
+	private int geoSirina;
+	private int geoDuziina;
+	
+	
+	public GeoLokacija(int geoSirina, int geoDuziina) {
+		super();
+		this.geoSirina = geoSirina;
+		this.geoDuziina = geoDuziina;
+	}
+
+
+	public int getGeoSirina() {
+		return geoSirina;
+	}
+
+
+	public int getGeoDuziina() {
+		return geoDuziina;
+	}
+	
+}

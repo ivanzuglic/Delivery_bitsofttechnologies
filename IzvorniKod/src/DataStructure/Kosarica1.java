d8faf75c51e0d423ede4f6a8d6712d20b3255bd7
Maja Matesic <maja.matesic@fer.hr>
2018-12-15 20:32:41 +0000

    Rad na podatkovnoj strukturi

---
+++ IzvorniKod/src/DataStructure/Kosarica1.java
@@ -0,0 +1,33 @@
+package DataStructure;
+
+public class Kosarica {
+
+    private int  ukupnaCijena;
+    private Map<Artikl, Integer> OdabraniProizvodi;
+
+    public void dodajArtikl(Artikl noviArtikl) {
+
+      //metoda dodaje artikl u kosaricu
+    }
+
+    public void promijeniKolicinu(Artikl artikl, int novaKolicina) {
+
+      //metoda mijenja kolicinu artikala u kosarici
+    }
+
+    public void ukloniArtikl(Artikl uklonjenArtikl) {
+
+      //metoda uklanja odredjeni artikl iz kosarice
+    }
+
+    public Narudzba finalizirajNarudzbu(GeoLokacija lokacijaDostave) {
+
+      //metoda za upis lokacije dostave na karti
+    }
+
+    public void clear() {
+
+      //metoda prazni kosaricu
+    }
+
+}

1984e4d23d3e1fa7af3a69706a68e2d83ecbb1c3
tonis <tonis@192.168.1.5>
2018-12-16 15:03:33 +0100

    Proba

---
+++ IzvorniKod/src/GUI/DefaultWindow.java
@@ -0,0 +1,23 @@
+package GUI;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+
+public class DefaultWindow extends JFrame{
+	public DefaultWindow() {
+		setSize(500, 480);
+	    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	    setTitle("Dostavljaona!");
+	}
+	
+	public static void main(String[] args) throws Exception {
+	    SwingUtilities.invokeAndWait(() -> {
+	      DefaultWindow window = new DefaultWindow();
+	      window.setLocation(20, 20);
+	      window.setVisible(true);
+	    });
+	  }
+
+}
